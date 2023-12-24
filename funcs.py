8a61b9ee6b0ae39d9ee2acc61f933c3e02f1cb5b
Дмитрий Васильев <quester@inbox.ru>
2023-12-24 16:49:35 +0400

    def minus

--- funcs.py
+++ funcs.py
@@ -5,4 +5,14 @@ def summ():
     print(f"Результат: {c}")
 
 
-summ()
+#summ()
+
+
+def minus():
+    a = int(input("Введите первое число: "))
+    b = int(input("Введите второе число: "))
+    c = a - b
+    print(f"Результат: {c}")
+
+
+minus()
